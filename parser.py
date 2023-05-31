9a65217284f9475e4fe02874201d8562ede7df3b
Santiago Arzaga <73621292+SantiagoArzaga@users.noreply.github.com>
2023-05-31 12:22:08 -0600

    Defined a data structure for the variables

--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ from ast import Sum, Sub, Mult, Div, Number, Write, Equal
 
 variable_table = {}
 values_table = {}
-
+data = []
 
 class Parser():
     def __init__(self):
@@ -31,9 +31,11 @@ class Parser():
         @self.pg.production('variabledeclaration : VAR assignment COLON type SEMI_COLON')
         def variable_declaration(p):
             #variable_table[values_table][p[3]] = None
-            for var_name in values_table:
-                values_table[var_name][p[3]] = None
-            print("values Table:", values_table)
+            #for var_name in values_table:
+            #    values_table[var_name][p[3]] = None
+            for row in data:
+                row['Type'] = p[3]
+            #print("values Table:", values_table)
             return
 
         @self.pg.production('assignment : LETTER COMMA assignment')
@@ -42,10 +44,14 @@ class Parser():
         def assignment(p):
             if len(p) > 2:
                 var_name = p[0].getstr()
-                values_table[var_name] = {}
+                #values_table[var_name] = {}
+                new_row = {'Variable': var_name, 'Type': None, 'Value': None}
+                data.append(new_row)
             elif p[0].gettokentype() == 'LETTER':
                 var_name = p[0].getstr()
-                values_table[var_name] = {}
+                #values_table[var_name] = {}
+                new_row = {'Variable': var_name, 'Type': None, 'Value': None}
+                data.append(new_row)
             else:
                 return
 
@@ -60,20 +66,38 @@ class Parser():
         def block(p):
             return p[2]
 
-        @self.pg.production('declaration : variable equal expression SEMI_COLON')
+
+        @self.pg.production('declaration : variable equal expression endsentence')
         def variable_assignation(p):
             #values_table[p[0].getstr()]["int"] = p[2]
+            #data[p[0].getstr()]['Value'] = p[2]
+            for row in data:
+                if row['Variable'] == p[0].getstr():
+                    row['Value'] = p[2]
+            for row in data:
+                print(row)
             print("value that reaches declaration : ", p[2])
-            print("variable that is assigned: ", values_table[p[0].getstr()])
+            #print("variable that is assigned: ", values_table[p[0].getstr()])
             return
         #@self.pg.production('declaration : IF expression THEN declaration declaration2')
         #@self.pg.production('declaration : WHILE expression DO declaration')
         #@self.pg.production('declaration : FOR OPEN_PAREN LETTER equal expression SEMI_COLON'
         #                    ' expression SEMI_COLON expression CLOSE_PAREN DO declaration')
         #@self.pg.production('declaration : declaration2')
-        @self.pg.production('declaration : WRITE OPEN_PAREN expression CLOSE_PAREN SEMI_COLON')
+        @self.pg.production('declaration : WRITE OPEN_PAREN expression CLOSE_PAREN endsentence')
         def write_statement(p):
+            for row in data:
+                if p[2].getstr() in row.values():
+                    print(row['Value'])
+                else:
+                    print("Variable not declared")
+            #print(p[2].getstr())
             return #Write(p[2])
+
+        @self.pg.production('endsentence : SEMI_COLON')
+        @self.pg.production('endsentence : SEMI_COLON declaration')
+        def end_sentence(p):
+            return p[0]
 
 
         #@self.pg.production('declaration2 : EMPTY')
@@ -88,7 +112,7 @@ class Parser():
         @self.pg.production('expression : simple')
         def expression(p):
             if len(p) == 1:
-                print("expression: ", p[0])
+                #print("expression: ", p[0])
                 return p[0]
             else:
                 return
@@ -107,7 +131,7 @@ class Parser():
                 elif operator.gettokentype() == 'SUB':
                     return Sub(left, right)
             else:
-                print("simple: ", p[0])
+                #print("simple: ", p[0])
                 return p[0]
 
 
@@ -124,19 +148,19 @@ class Parser():
                 elif operator.gettokentype() == 'DIV':
                     return Div(left, right)
             else:
-                print("term: ", p[0])
+                #print("term: ", p[0])
                 return p[0]
 
         @self.pg.production('factor : NUMBER')
         def number(p):
-            print("factor", p[0])
+            #print("factor", p[0])
             return p[0].value
 
         @self.pg.production('variable : LETTER')
         @self.pg.production('factor : LETTER')
         def variable(p):
             var_name = p[0]
-            print("variable", var_name)
+            #print("variable", var_name)
             return var_name
 
         @self.pg.production('equal : COLON EQUAL')
